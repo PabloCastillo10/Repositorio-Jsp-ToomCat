aff53ef1b1c37ff84d4ceabe5b2ff633ca6d142c
PabloCastillo10 <pablocastillo4821@gmail.com>
2024-07-09 11:57:37 -0600

    cambios 9/07/2024

---
+++ src/main/java/org/pablocastillo/webapp/util/JpaUtil.java
@@ -0,0 +1,29 @@
+package org.pablocastillo.webapp.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+    private static final EntityManagerFactory emf = buildEntityManagerFactory();
+        
+    public static EntityManagerFactory buildEntityManagerFactory(){
+        try{
+            return Persistence.createEntityManagerFactory("SGBDProductoIN5CM");
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    
+    public static void close() {
+        emf.close();
+    }
+    
+}
+
+    
